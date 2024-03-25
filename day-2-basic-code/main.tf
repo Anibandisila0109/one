097693745a62d34fbe7a805e581dc59ca83b29ae
Anibandisila0109 <162276852+Anibandisila0109@users.noreply.github.com>
2024-03-25 21:53:03 +0530

    Update main.tf

--- day-2-basic-code/main.tf
+++ day-2-basic-code/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "ec2" {
     ami = var.ami_id
     instance_type = var.instance_type
+    subnet_id = var.subnet_id
     key_name = var.key_name
     tags = {
       Name = "demo"
