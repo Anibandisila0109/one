b57d11eef0b85507a21bf0e7d0e3c312000afd95
anitha <anithab0109@gmail.com>
2024-03-25 22:10:15 +0530

    jhgffghj

---
+++ day-9-modules/variable.tf
@@ -0,0 +1,20 @@
+variable "ami_id" {
+    description = "passing AMI value to main.tf"
+    type = string
+    default = ""
+  
+}
+variable "instance_type" {
+    type = string
+    default = ""
+  
+}
+variable "key_name" {
+    type = string
+    default = ""
+  
+}
+variable "tags" {
+    default = ""
+  
+}
