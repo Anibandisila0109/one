9b3605d1ea4c8e4074820a4bcd997c7f6ad8530f
Anibandisila0109 <162276852+Anibandisila0109@users.noreply.github.com>
2024-03-26 12:06:48 +0530

    Update variable.tf

--- day-2-basic-code/variable.tf
+++ day-2-basic-code/variable.tf
@@ -1,16 +1,16 @@
 variable "ami_id" {
     description = "passing AMI value to main.tf"
     type = string
-    default = ""
+    default = "ami-033a1ebf088e56e81"
   
 }
 variable "instance_type" {
     type = string
-    default = ""
+    default = "t2.micro"
   
 }
 variable "key_name" {
     type = string
-    default = ""
+    default = "linux"
   
 }
