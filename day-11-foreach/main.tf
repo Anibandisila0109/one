d82d20844c5c997fad32998380ca931b508966a7
anitha <anithab0109@gmail.com>
2024-03-27 20:21:29 +0530

    jhgfghj

---
+++ day-11-foreach/main.tf
@@ -0,0 +1,27 @@
+variable "ami" {
+    type = string
+    default = "ami-033a1ebf088e56e81"
+  
+}
+ variable "instance_type" {
+    type = string
+    default = "t2.micro"
+   
+ }
+  variable "sandboxes" {
+    type = list(string)
+    default = [ "dev","test" ]
+    
+  }
+
+
+  resource "aws_instance" "sandboxes" {
+    ami = "ami-033a1ebf088e56e81"
+    instance_type = var.instance_type
+    for_each = toset(var.sandboxes)
+   # count = length(var.sandboxes)
+    tags = {
+       Name = "each.value"
+    }
+    
+  }
